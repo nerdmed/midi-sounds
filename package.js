96fd6c8c2b2116ffb25be0dc676c6da8723df867
Ahmed Hassan <a.hassan@buismed.de>
2016-04-06 17:04:04 +0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'midi-sound',
-    version: '0.0.5',
+    version: '0.0.6',
 
     // Brief, one-line summary of the package.
     summary: 'Plays MIDI Sounds in the Browser',
